71a2f276b393b3bf9a0351feae94a1ccbcf8d00f
Alex <alexbaturin@list.ru>
2026-08-13 00:18:55 +0800

    Отладка файлового хранилища

--- src/main/java/ru/skypro/homework/config/AlfrescoConfig.java
+++ src/main/java/ru/skypro/homework/config/AlfrescoConfig.java
@@ -3,12 +3,13 @@ package ru.skypro.homework.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 @EnableConfigurationProperties(AlfrescoProperties.class)
 public class AlfrescoConfig {
-
+    /*
     @Bean
     public RestClient alfrescoRestClient(
             AlfrescoProperties properties
@@ -21,6 +22,27 @@ public class AlfrescoConfig {
                                 properties.password()
                         )
                 )
+                .build();
+    }
+    */
+
+
+    @Bean
+    public RestClient alfrescoRestClient(
+            AlfrescoProperties properties
+    ) {
+        return RestClient.builder()
+                .baseUrl(properties.url())
+                .defaultRequest(request -> {
+                    // Создаём строку для Basic Auth
+                    String auth = properties.username() + ":" + properties.password();
+                    String encodedAuth = java.util.Base64.getEncoder()
+                            .encodeToString(auth.getBytes());
+
+                    request.headers(headers -> {
+                        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuth);
+                    });
+                })
                 .build();
     }
 }
