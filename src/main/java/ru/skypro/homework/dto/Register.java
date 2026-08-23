42862454d3f83a0835fcf1789a7cd42153005668
Alex <alexbaturin@list.ru>
2026-08-23 21:54:16 +0800

    Удалил лишние проверки

--- src/main/java/ru/skypro/homework/dto/Register.java
+++ src/main/java/ru/skypro/homework/dto/Register.java
@@ -130,30 +130,4 @@ public record Register(
         Role role
 
 ) {
-
-    /**
-     * Компактный конструктор record, выполняющий дополнительную проверку на null и пустые значения.
-     * Эта проверка дублирует часть валидации, заданной аннотациями, и служит дополнительной защитой
-     * от некорректных данных на уровне создания экземпляра.
-     */
-    public Register {
-        if (username == null || username.isBlank()) {
-            throw new IllegalArgumentException("Логин пользователя не может быть пустым!");
-        }
-        if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException("Пароль не может быть пустым!");
-        }
-        if (firstName == null || firstName.isBlank()) {
-            throw new IllegalArgumentException("Имя пользователя не может быть пустым!");
-        }
-        if (lastName == null || lastName.isBlank()) {
-            throw new IllegalArgumentException("Фамилия пользователя не может быть пустой!");
-        }
-        if (phone == null || phone.isBlank()) {
-            throw new IllegalArgumentException("Телефон пользователя не может быть пустым!");
-        }
-        if (role == null) {
-            throw new IllegalArgumentException("Роль пользователя не может быть пустым значением!");
-        }
-    }
 }
